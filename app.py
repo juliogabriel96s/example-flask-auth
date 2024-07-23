8c8be9ec1c6663dd2aebbaae65b4605f7b616c36
juliogabriel96s <juliogabrielsetor96@gmail.com>
2024-07-23 15:22:31 -0300

    error flask shell

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
@@ -13,3 +13,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
